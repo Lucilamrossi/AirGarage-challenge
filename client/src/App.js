f016589779993c4bdbd6f92ce462a9b797f69bd6
Lucilamrossi <lucilamrossi@gmail.com>
2021-07-21 22:00:39 -0300

    feat(client /SearchBar): Search component with Google API and MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './components/Home/Home'
+import SearchBar from './components/SearchBar/SearchBar'
 
 function App() {
   return (
     <div>
       <Home/>
+      <SearchBar/>
     </div>
   );
 }
