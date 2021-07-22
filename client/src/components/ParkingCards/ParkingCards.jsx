be9688b61f6fe14b53d924456016199573263428
Lucilamrossi <lucilamrossi@gmail.com>
2021-07-22 04:29:45 -0300

    feat(client /ParkingCards): Add Loading process

--- client/src/components/ParkingCards/ParkingCards.jsx
+++ client/src/components/ParkingCards/ParkingCards.jsx
@@ -3,11 +3,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getParkings } from '../../redux/actions/getParkings';
 
 import Pagination from '@material-ui/lab/Pagination';
-
+import LinearProgress from '@material-ui/core/LinearProgress';
 import ParkingCard from './ParkingCard/ParkingCard';
 
 export default function ParkingCards() {
-  const {parkings, queries, totalPages} = useSelector((state) => state);
+  const {parkings, queries, totalPages, loading} = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const handleChange = (event, value) => {
@@ -16,11 +16,17 @@ export default function ParkingCards() {
 
   return (
     <>
-    <Pagination count={totalPages} page={queries?.page || 1} onChange={handleChange} />
-      {parkings.map((parkingLot) => {
-        console.log(parkingLot);
-        return <ParkingCard parkingLot={parkingLot} />;
-      })}
+    {parkings.length > 0 && (
+      <>
+        <Pagination color="secondary" count={totalPages} page={queries?.page || 1} onChange={handleChange} />
+        {parkings.map((parkingLot) => {
+          console.log(parkingLot);
+          return <ParkingCard parkingLot={parkingLot} />;
+        })}
+      </>)
+    }
+    {loading && <LinearProgress color="secondary" />}
+    
     </>
   );
 }
