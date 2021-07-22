22bcb547bd343cbbe1674ff3165d21f270c8647c
Lucilamrossi <lucilamrossi@gmail.com>
2021-07-22 18:48:32 -0300

    feat(client /header): Create and styled Header

---
+++ client/src/components/Header/styles.js
@@ -0,0 +1,26 @@
+import { makeStyles } from '@material-ui/core/styles';
+import theme from '../../utils/theme';
+
+export const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-arround'
+  },
+  toolbar: {
+    minHeight: '8rem',
+    backgroundColor: theme.palette.primary.light,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    color: theme.palette.primary.main,
+    padding: '0 1rem'
+  },
+  media: {
+    minHeight: 50,
+    width: 150,
+    margin: '0 1rem'
+  },
+});
+
