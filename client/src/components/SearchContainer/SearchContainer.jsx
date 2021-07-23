d3a9a25d707621340fdecd5f1d0167eeac141bcc
Lucilamrossi <lucilamrossi@gmail.com>
2021-07-23 03:08:04 -0300

    feat(client /SearchContainer): Styles

--- client/src/components/SearchContainer/SearchContainer.jsx
+++ client/src/components/SearchContainer/SearchContainer.jsx
@@ -1,19 +1,21 @@
-import { useStyles } from './styles';
-import SearchBar from '../SearchBar/SearchBar';
-import { Typography } from '@material-ui/core';
+import React from 'react';
 
-function SearchContainer() {
+import SearchBar from '../SearchBar/SearchBar';
+
+import { Typography } from '@material-ui/core';
+import { useStyles } from './styles';
+
+export default function SearchContainer() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <div className={classes.searchContainer}>
-        <Typography className={classes.searchTitle}>Search for parking lots here!</Typography>
+        <Typography className={classes.searchTitle}>
+          Search for parking lots here!
+        </Typography>
         <SearchBar />
       </div>
     </div>
-
   );
 }
-
-export default SearchContainer;
--- client/src/components/SearchContainer/styles.js
+++ client/src/components/SearchContainer/styles.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 import theme from '../..//utils/theme';
 
-import Image from '../../utils/img/backgroud-texture.png'
+import Image from '../../utils/img/backgroud-texture.png';
 
 export const useStyles = makeStyles({
   root: {
@@ -10,7 +10,7 @@ export const useStyles = makeStyles({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: '2rem'
+    padding: '2rem',
   },
   searchContainer: {
     display: 'flex',
@@ -18,6 +18,7 @@ export const useStyles = makeStyles({
     alignItems: 'center',
     justifyContent: 'center',
     border: `1px solid ${theme.palette.primary.main}`,
+    borderRadius: '.7rem',
     padding: '2rem',
     backgroundColor: theme.palette.primary.contrastText,
     transition: '.5s ease',
@@ -25,11 +26,12 @@ export const useStyles = makeStyles({
       boxShadow: '0px 0px 30px 0px rgba(0,0,0,0.4)',
       webkitBoxShadow: ' 0px 0px 30px 0px rgba(0,0,0,0.4)',
       mozBoxShadow: ' 0px 0px 30px 0px rgba(0,0,0,0.4)',
-    }
+    },
   },
   searchTitle: {
     fontSize: '2rem',
     paddingBottom: '2rem',
-    color: theme.palette.primary.main
-  }
+    color: theme.palette.primary.main,
+    textAlign: 'center',
+  },
 });
